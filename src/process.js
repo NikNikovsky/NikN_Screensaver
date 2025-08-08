5ccf06c1c080d7873fa0346d0a195b3d5736815d
NikNikovsky <zejdzztegomaila@gmail.com>
2025-08-08 15:42:15 +0200

    it still crashes
    Signed-off-by: NikNikovsky <zejdzztegomaila@gmail.com>

--- src/process.js
+++ src/process.js
@@ -77,7 +77,17 @@ class proc extends ThirdPartyAppProcess {
         // --- Registering Alt+I accelerator using acceleratorStore ---
         if (this.acceleratorStore && Array.isArray(this.acceleratorStore)) {
             this.acceleratorStore.push({
-                // You may want to add accelerator definitions here if needed
+                alt: true,
+                key: "i",
+                action: (proc, event) => {
+                    if (this._u === 0 && this._l === 0) {
+                        this.showSecretCodeInputOverlay();
+                   }
+
+                },
+
+
+                global: true
             });
         }
 
